467f43572280be78fa96c07aafb4b988e3663a16
Phillip <phillip.lam0@gmail.com>
2022-11-23 15:00:57 -0500

    feat(layout): adding Layout component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,29 @@
+// components/Layout.tsx
+
+import Head from "next/head";
+import { Footer } from "../Footer";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor | Interactive Comments Section</title>
+        <meta
+          name="description"
+          content="Frontend Mentor - Interactive Comments Section"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="min-h-screen h-full mx-4 md:mx-auto flex flex-col justify-between">
+          <div>{children}</div>
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+}
