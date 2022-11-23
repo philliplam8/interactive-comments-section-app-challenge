4168d3c13b22b35c5192877ee8c5364ad49e35cc
Phillip <phillip.lam0@gmail.com>
2022-11-23 14:59:43 -0500

    feat: adding NextJS document page for fonts and overall background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      slate: colors.slate,
+      moderateBlue: 'hsl(238, 40%, 52%)',
+      softRed: 'hsl(358, 79%, 66%)',
+      lightGrayishBlue: 'hsl(239, 57%, 85%)',
+      paleRed: 'hsl(357, 100%, 86%)',
+      darkBlue: 'hsl(212, 24%, 26%)',
+      grayishBlue: 'hsl(211, 10%, 45%)',
+      lightGray: 'hsl(223, 19%, 93%)',
+      veryLightGray: 'hsl(228, 33%, 97%)'
+    },
     extend: {},
   },
   plugins: [],
