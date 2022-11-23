a36290b4f079735f25a39a9bdefd171431173734
Phillip <phillip.lam0@gmail.com>
2022-11-23 16:33:12 -0500

    refactor(comment): abstracting card footer

--- components/Comment/Comment.tsx
+++ components/Comment/Comment.tsx
@@ -1,16 +1,16 @@
 import { Card } from "../Card";
-import { Plus, Minus } from "../Buttons";
+import { Plus, Minus, Reply } from "../Buttons";
 
 export default function Comment(): JSX.Element {
   function CardHeader(props: {
     avatar: string;
-    name: string;
+    username: string;
     time: string;
   }): JSX.Element {
     return (
       <div className="flex flex-row gap-4">
         <div>Avatar</div>
-        <h1 className="font-medium text-darkBlue">{props.name}</h1>
+        <h1 className="font-medium text-darkBlue">{props.username}</h1>
         <div>{props.time}</div>
       </div>
     );
@@ -18,10 +18,19 @@ export default function Comment(): JSX.Element {
 
   function Votes(): JSX.Element {
     return (
-      <div className="w-[100px] h-[35px] sm:w-[40px] sm:h-[100px] px-4 py-5 sm:p-2 flex flex-row sm:flex-col justify-between items-center bg-veryLightGray text-moderateBlue font-medium rounded-xl">
+      <div className="w-[100px] h-[35px] sm:w-[40px] sm:h-[100px] px-4 py-5 sm:px-2 sm:py-3 flex flex-row sm:flex-col justify-between items-center bg-veryLightGray text-moderateBlue font-medium rounded-xl">
         <Plus />
         <div>12</div>
         <Minus />
+      </div>
+    );
+  }
+
+  function CardFooterMobile(): JSX.Element {
+    return (
+      <div className="sm:hidden flex flex-row justify-between">
+        <Votes />
+        <Reply />
       </div>
     );
   }
@@ -33,7 +42,16 @@ export default function Comment(): JSX.Element {
           <Votes />
         </div>
         <div className="flex flex-col sm:flex-col gap-4">
-          <CardHeader avatar={""} name={"amyrobson"} time={"1 month ago"} />
+          <div className="flex flex-row justify-between">
+            <CardHeader
+              avatar={""}
+              username={"amyrobson"}
+              time={"1 month ago"}
+            />
+            <div className="hidden sm:block">
+              <Reply />
+            </div>
+          </div>
 
           <div id="comment">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
@@ -41,9 +59,7 @@ export default function Comment(): JSX.Element {
             assumenda, et porro! Doloremque, repellat. Dolor fuga et nam quos?
           </div>
 
-          <div className="sm:hidden block">
-            <Votes />
-          </div>
+          <CardFooterMobile />
         </div>
       </div>
     </Card>
