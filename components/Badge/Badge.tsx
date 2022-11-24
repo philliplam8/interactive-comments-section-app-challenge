63c9082dbd3e8c21c1818bebce941eae4fd63aa5
Phillip <phillip.lam0@gmail.com>
2022-11-23 20:11:07 -0500

    feat(badge): add "you" Badge component

---
+++ components/Badge/Badge.tsx
@@ -0,0 +1,11 @@
+export default function Badge(): JSX.Element {
+  return (
+    <div
+      className={
+        "-ml-2 mb-0.5 mt-0.5 px-[7.5px] flex items-center justify-center bg-moderateBlue rounded-sm"
+      }
+    >
+      <p className="text-[0.78em] text-white">you</p>
+    </div>
+  );
+}
