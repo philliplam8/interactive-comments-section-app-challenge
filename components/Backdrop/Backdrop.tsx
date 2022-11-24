6477b49e134d75f13672e1247ef3a9f9a6aefbc1
Phillip <phillip.lam0@gmail.com>
2022-11-24 11:31:48 -0500

    feat(backdrop): convert backdrop into parent container

--- components/Backdrop/Backdrop.tsx
+++ components/Backdrop/Backdrop.tsx
@@ -1,13 +1,12 @@
 export default function Backdrop(props: {
-  show: any;
-  closeModal: any;
+  show: boolean;
+  children: React.ReactNode;
 }): JSX.Element {
   return (
     <>
-      <div
-        className="h-screen w-screen absolute top-0 left-0 bg-black/90 z-20"
-        onClick={props.closeModal}
-      ></div>
+      <div className="h-screen w-screen px-4 absolute top-0 left-0 right-0 flex justify-center items-center bg-grayishBlue/70 z-20">
+        {props.children}
+      </div>
     </>
   );
 }
