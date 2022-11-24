b49206086140d27ec90c1b3f1a6b696a8251eba0
Phillip <phillip.lam0@gmail.com>
2022-11-24 11:32:17 -0500

    feat(modal): wrap modal content within backdrop container

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "../UI/Card";
 import { SecondaryButton, ErrorButton } from "../UI/Buttons";
 import { Backdrop } from "../Backdrop";
@@ -19,15 +19,18 @@ export default function Modal(): JSX.Element {
 
   function ModalContent(): JSX.Element {
     return (
-      <div className="absolute top-0 left-0 right-0 w-full max-w-[730px] mx-auto z-30">
+      <div className="w-full max-w-[400px] z-30">
         <Card>
           <div className="flex flex-col gap-4 p-2">
             <h1 className="text-xl sm:text-2xl text-darkBlue font-medium ">
               Delete comment
             </h1>
             <p className="text-grayishBlue">{TEXT.DELETE_CONFIRMATION}</p>
-            <div className="flex flex-row gap-3 justify-start min-[375px]:justify-between">
-              <SecondaryButton label={TEXT.BTN_NO} />
+            <div className="flex flex-col min-[370px]:flex-row gap-3 justify-between">
+              <SecondaryButton
+                label={TEXT.BTN_NO}
+                handleClick={handleModalToggle}
+              />
               <ErrorButton label={TEXT.BTN_YES} />
             </div>
           </div>
@@ -36,12 +39,20 @@ export default function Modal(): JSX.Element {
     );
   }
 
+  // Disable vertical scrolling when the modal is open
+  useEffect((): void => {
+    showModal
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  });
+
   return (
     <>
       {showModal && (
         <>
-          <ModalContent />
-          <Backdrop show={showModal} closeModal={handleModalToggle} />
+          <Backdrop show={showModal}>
+            <ModalContent />
+          </Backdrop>
         </>
       )}
     </>
