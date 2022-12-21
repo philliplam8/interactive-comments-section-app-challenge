dd749f563d3d42f37df741eeb9625d5603147638
Phillip <phillip.lam0@gmail.com>
2022-12-21 14:35:03 -0500

    fix(accessibility): update hamburger menu with aria label

--- components/UI/Drawer/Drawer.tsx
+++ components/UI/Drawer/Drawer.tsx
@@ -38,7 +38,7 @@ export default function Drawer({
 
   return (
     <>
-      <button onClick={handleMenuClick}>
+      <button onClick={handleMenuClick} aria-label="Open hamburger menu">
         <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z"
