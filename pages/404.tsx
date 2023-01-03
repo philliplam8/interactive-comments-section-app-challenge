79dc7ea2288cfb43e899a82d7309d1c43d1265f2
Phillip <phillip.lam0@gmail.com>
2023-01-03 11:08:38 -0500

    refactor(404): moving hardcoded text into object

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,6 +3,12 @@ import { Layout } from "../components/Layout";
 import { MdOutlineWrongLocation } from "react-icons/md";
 import { PrimaryButton } from "../components/UI/Buttons";
 
+const ERROR_TEXT = {
+  heading: `Oops! Where in the world did we go...`,
+  subtitle: `Sorry we couldn't find what you were looking for.`,
+  button: `Let's Go Home`,
+};
+
 export default function Custom404() {
   const route = useRouter();
   const handleHomeButton = () => {
@@ -13,19 +19,18 @@ export default function Custom404() {
     <Layout>
       <div className="flex flex-col gap-10 justify-center text-center items-center text-grayishBlue dark:text-slate-400 ">
         <div className="flex flex-col gap-5 items-center">
-          <MdOutlineWrongLocation className="h-10 w-10 text-softRed" />
+          <MdOutlineWrongLocation className="h-10 w-10 text-softRed animate-bounce" />
           <div>
-            <h1 className="text-2xl font-semibold ">
-              Oops! Where in the world did we go...
-            </h1>
+            <h1 className="text-2xl font-semibold ">{ERROR_TEXT.heading}</h1>
             <div>
-              <h2 className="text-sm font-light">
-                Sorry we couldn't find what you were looking for.
-              </h2>
+              <h2 className="text-sm font-light">{ERROR_TEXT.subtitle}</h2>
             </div>
           </div>
         </div>
-        <PrimaryButton label={"Let's Go Home"} handleClick={handleHomeButton} />
+        <PrimaryButton
+          label={ERROR_TEXT.button}
+          handleClick={handleHomeButton}
+        />
       </div>
     </Layout>
   );
